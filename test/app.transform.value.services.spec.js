a269ab10799478a6e82620435dd3c4249c9bc26d
wolfather <so.israelweb@gmail.com>
2016-02-03 21:25:10 -0200

    removendo teste do package.son

--- test/app.transform.value.services.spec.js
+++ test/app.transform.value.services.spec.js
@@ -3,32 +3,32 @@
 describe('Testing the app.transform.value.js service', function() {
 	beforeEach(angular.mock.module('app'))
 
-	it('should have a working transformvalue first name service', inject(['transformvalue', 
-		function(transformvalue) {
+	it('should be working transformvalue first name service', 
+		inject(['transformvalue', function(transformvalue) {
 			expect(transformvalue.name(user.fullName).first()).toBe('israel');
 		}
 	]))
 
-	it('should have a working transformvalue lastName service', inject(['transformvalue', 
-		function(transformvalue) {
+	it('should be working transformvalue lastName service', 
+		inject(['transformvalue', function(transformvalue) {
 			expect(transformvalue.name(user.fullName).last()).toBe('soares');
 		}
 	]))
 
-	it('should have a working transformvalue lastName service', inject(['transformvalue', 
-		function(transformvalue) {
+	it('should be working transformvalue.phone(param).countryCode() service', 
+		inject(['transformvalue', function(transformvalue) {
 			expect(transformvalue.phone(user.cellphoneTypeOne).countryCode()).toBe('55');
 		}
 	]))
 
-	it('should have a working transformvalue lastName service', inject(['transformvalue', 
-		function(transformvalue) {
+	it('should be working transformvalue.phone(param).areaCode() service', 
+		inject(['transformvalue', function(transformvalue) {
 			expect(transformvalue.phone(user.cellphoneTypeOne).areaCode()).toBe('11');
 		}
 	]))
 
-	it('should have a working transformvalue lastName service', inject(['transformvalue', 
-		function(transformvalue) {
+	it('should be working transformvalue.phone(param).number() service', 
+		inject(['transformvalue', function(transformvalue) {
 			expect(transformvalue.phone(user.cellphoneTypeOne).number()).toBe('123456789');
 		}
 	]))
--- test/app.validate.services.spec.js
+++ test/app.validate.services.spec.js
@@ -3,40 +3,38 @@
 describe('Testing the app.validate.services.js', function() {
 	beforeEach(angular.mock.module('app'))
 
-	it('should have a working validateservice email service', inject(['validateservice', 
-		function(validateservice) {
+	it('should have a working validateservice email service', 
+		inject(['validateservice', function(validateservice) {
 			expect(validateservice.validate.email.test(user.emailDotComDotBr)).toBeTruthy()
 		}
-
 	]))
 
-	it('should have a working validateservice CPF service', inject(['validateservice', 
-		function(validateservice) {
+	it('should have a working validateservice CPF service', 
+		inject(['validateservice', function(validateservice) {
 			expect(validateservice.validate.cpf.test(user.cpf)).toBeTruthy()
 		}
-
 	]))
 	
-	it('Validate the cellphone validation like 55-11-12345-6789', inject(['validateservice', 
-		function(validateservice) {
+	it('Validate the cellphone validation like 55-11-12345-6789', 
+		inject(['validateservice', function(validateservice) {
 			expect(validateservice.validate.cellPhone.test(user.cellphoneTypeOne)).toBeTruthy()
 		}
 	]))
 
-	it('Validate the cellphone validation like 55-11-1234-5678', inject(['validateservice', 
-		function(validateservice) {
+	it('Validate the cellphone validation like 55-11-1234-5678', 
+		inject(['validateservice', function(validateservice) {
 			expect(validateservice.validate.cellPhone.test(user.cellphoneTypeTwo)).toBeTruthy()
 		}
 	]))
 
-	it('Validate the email validation without .br in the end', inject(['validateservice', 
-		function(validateservice) {
+	it('Validate the email validation without .br in the end', 
+		inject(['validateservice', function(validateservice) {
 			expect(validateservice.validate.email.test(user.emailDotCom)).toBeTruthy()
 		}
 	]))
 
-	it('Validate the email validation with .br in the end', inject(['validateservice', 
-		function(validateservice) {
+	it('Validate the email validation with .br in the end', 
+		inject(['validateservice', function(validateservice) {
 			expect(validateservice.validate.email.test(user.emailDotComDotBr)).toBeTruthy()
 		}
 	]))
